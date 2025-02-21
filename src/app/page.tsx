daafff134825b3533f980b1f983ede5f8b298e67
kauaklein <kaua1floripa@gmail.com>
2025-02-21 14:47:17 -0300

    ajuste na biblioteca

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
 import { OnHeader } from "@/components/navbar";
 // import { Simple } from "@/components/formfield";
 import { Sitemap } from "@/components/footer";
+import { Simple } from "@/components/carousel";
 
 export default function Home() {
 return(
 
-    <><section><OnHeader></OnHeader></section>
-    <main><Sitemap></Sitemap></main></>
+    <>
+    
+    
+    <section><OnHeader></OnHeader></section>
+
+    <main><Simple></Simple><Simple></Simple></main>
+    
+    
+    <footer><Sitemap></Sitemap></footer>
+    </>
     
 )
  
